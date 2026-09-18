7ff87af21d228ee65b1dab953bd8c3283e4de4e0
ASHWIN JADHAV <ashwinj2110@gmail.com>
2026-09-18 17:42:51 +0530

    First code - Pdf data extraction

---
+++ recruiter_agent.py
@@ -0,0 +1,16 @@
+from pypdf import PdfReader
+
+pdf_path = "candidates/ready_for_testing.pdf"
+
+reader = PdfReader(pdf_path)
+
+text = ""
+
+for page in reader.pages:
+    text += page.extract_text() + "\n"
+
+print("==========================================")
+print("              CV CONTENT                  ")
+print("==========================================")
+
+print(text+"\n")
